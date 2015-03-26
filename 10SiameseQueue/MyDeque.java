98e180f8a3f6a64a23bcc0e1b11945c4ebb2a78f
Shawn Li <shawnli050298@gmail.com>
2015-03-25 22:20:22 -0400

    I have been conquered. I will continue tomorrow

--- 10SiameseQueue/MyDeque.java
+++ 10SiameseQueue/MyDeque.java
@@ -1,46 +1,135 @@
-public class MyDeue{
+public class MyDeque<T>{
     private T[] Queue;
     private int head;
     private int tail;
+    public static void main(String[]args){
+	MyDeque A = new MyDeque(0);
+	A.addFirst(5);
+	A.addLast(3);
+	System.out.println(A.getLast());
+	System.out.println(A.getFirst());
+	A.addFirst(4);
+	A.addFirst(7);
+	System.out.println(A.getFirst());
+	System.out.println(A.removeFirst());
+	System.out.println(A.removeFirst());
+	System.out.println(A.getFirst());
+	A.addFirst(6);
+	System.out.println(A.getFirst());
+    }
     public MyDeque(){
     }
     public MyDeque(int size){
-	Queue = new T[size];
+	Queue = (T[])new Object[size];
+	tail = 0;
+	if (size > 1){
+	    tail = size - 1;
+	}
 	head = 0;
-	tail = size - 1;
     }
     public void addFirst(T newHead){
-	if (head == tail){
-	    Queue[head] = newHead;
+	if (Queue.length == 0){
+	    resize(Queue);
+	    Queue[0] = newHead;
 	}
-	else if ((head == 0 && tail == Queue.length - 1) || head - tail == 1){
+	else{
 	    resize(Queue);
 	    Queue[Queue.length - 1] = newHead;
 	    head = Queue.length - 1;
 	    tail = Queue.length - 2;
 	}
-	    
+	
     }
-    public void addLast(T lastHead){
+    public void addLast(T newTail){
+	if (Queue.length == 0){
+	    resize(Queue);
+	    Queue[0] = newTail;
+	}
+	else{
+	    resize(Queue);
+	    Queue[Queue.length - 1] = newTail;
+	    head = 0;
+	    tail = Queue.length - 1;
+	}
     }
     public T removeFirst(){
+    	T returnT = getFirst();
+    	if (head == Queue.length - 1){
+    	    head = 0;
+    	}
+    	else{
+    	    head += 1;
+    	}
+	resize2(Queue);
+	head = 0;
+	tail = Queue.length - 1;
+	return returnT;
     }
-    public T removeLast(){
+    //public T removeLast(){
+    //}
+    public T getFirst(){
+	return Queue[head];
+    }
+    public T getLast(){
+	return Queue[tail];
     }
     public void resize(T[] queue){
-	T[] newQueue = new T[Queue.length + 1];
-	int counter = 0;
-	while (head + counter < Queue.length){
-	    newQueue[counter] = Queue[head + counter];
-	    counter += 1;	
+	T[] newQueue;
+	int length;
+	if (head == 0){
+	    newQueue = (T[])new Object[(Queue.length + 1)];
+	    length = Queue.length;
 	}
-	int counter2 = 0;
-	while (tail - counter2 >= 0){
-	    newQueue[counter] = Queue[counter2];
-	    counter += 1;
-	    counter2 += 1;
+	else{
+	    newQueue = (T[])new Object[(Queue.length - head) + tail + 1 ];
+	    length = (Queue.length - head) + tail;
+	}
+	if (queue.length == 0){
+	}
+	else{
+	    int counter = 0;
+	    while (head + counter < Queue.length){
+		newQueue[counter] = Queue[head + counter];
+		counter += 1;	
+	    }
+	    int counter2 = 0;
+	    while (tail - counter2 >= 0){
+		newQueue[counter] = Queue[counter2];
+		counter += 1;
+		counter2 += 1;
+		head = 0;
+		tail = Queue.length - 2;
+	    }
 	}
 	Queue = newQueue;
     }
-    
+    public void resize2(T[] queue){
+	T[] newQueue;
+	int counter = 0;
+	newQueue = (T[])new Object[(Queue.length - 1)];
+	if (queue.length == 0){
+	}
+	else if (tail == Queue.length - 2){
+	    while (head + counter <= tail){
+		newQueue[counter] = Queue[counter];
+		counter += 1;
+	    }
+	}
+	else{
+	    while (head + counter < Queue.length){
+		newQueue[counter] = Queue[head + counter];
+		counter += 1;	
+	    }
+	    int counter2 = 0;
+	    while (tail - counter2 >= 0){
+		newQueue[counter] = Queue[counter2];
+		counter += 1;
+		counter2 += 1;
+		head = 0;
+		tail = Queue.length - 2;
+	    }
+	
+	}
+	Queue = newQueue;
+    }
 }
