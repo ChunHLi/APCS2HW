b6c527411937a02f4920ec237b8bd37888c46f2e
Shawn Li <shawnli050298@gmail.com>
2015-03-30 22:26:36 -0400

    I don't care if he isn't real, Sasquatch is really big and really friendly.

---
+++ 11TheBigFriendlySasquatch/MyBFS.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class MyBFS{
+
+    ArrayDeque<OP> web;
+    char[][] grid;
+
+    public static void main(String[]args){
+    }
+
+    public MyBFS(){
+    }
+
+    public MyBFS(char[][] Grid){
+    }
+
+}
---
+++ 11TheBigFriendlySasquatch/OP.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class OP{
+    int row;
+    int col;
+
+    public OP(int Row, int Col){
+	row = Row;
+	col = Col;
+    }
+    
+    public int getRow(){
+	return row;
+    }
+    
+    public int getCol(){
+	return col;
+    }
+
+    public void setRow(int Row){
+	row = Row;
+    }
+    
+    public void setCol(int Col){
+	col = Col;
+    }
+
+}
