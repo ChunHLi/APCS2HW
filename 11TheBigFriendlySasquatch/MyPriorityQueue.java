aa8c85d738bcf705ebf06f3e3351dfac457bae99
Shawn Li <shawnli050298@gmail.com>
2015-04-14 18:14:29 -0400

    Yay priorityQueue, not that bad!

---
+++ 11TheBigFriendlySasquatch/MyPriorityQueue.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MyPriorityQueue<T>{
+    ArrayDeque<T> PriorityQueue;
+    LinkedList<Integer> PriorityList;
+    
+    public MyPriorityQueue(){
+	PriorityQueue = new ArrayDeque<T>();
+	PriorityList =  new LinkedList<Integer>();
+    }
+
+    public void add(T t, int n){
+	LinkedList<T> PriorityQueueList = new LinkedList<T>(PriorityQueue);
+	if (PriorityQueueList.size() == 0){
+	    PriorityQueueList.add(t);
+	    PriorityList.add(n);
+	}
+	else{
+	    int counter = 0;
+	    while (counter <= PriorityQueueList.size()){
+		if (counter == PriorityQueueList.size()){
+		    PriorityQueueList.add(t);
+		    PriorityList.add(n);
+		    break;
+		}
+		if (n > PriorityList.get(counter)){
+		    PriorityQueueList.add(counter,t);
+		    PriorityList.add(counter,n);
+		    break;
+		}
+		counter += 1;
+	    }
+	}
+	PriorityQueue = new ArrayDeque<T>(PriorityQueueList);
+    }
+    
+}
