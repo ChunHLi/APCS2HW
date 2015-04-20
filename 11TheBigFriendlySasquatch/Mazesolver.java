e34156e79b9234d1cd4809bcdcd8c4ab93e14b4e
Shawn Li <shawnli050298@gmail.com>
2015-04-19 20:46:52 -0400

    Added A* and Best

--- 11TheBigFriendlySasquatch/Mazesolver.java
+++ 11TheBigFriendlySasquatch/Mazesolver.java
@@ -1,25 +1,25 @@
 import java.util.*;
 import java.io.*;
-
 public class Mazesolver{
     private char[][]maze;
     private int maxx,maxy;
-    private int startx,starty;
+    private int startx,starty,endx,endy;
     //Terminal keycodes to clear the terminal, or hide/show the cursor
-    private String clear =  "\033[2J";
-    private String hide =  "\033[?25l";
-    private String show =  "\033[?25h";
+    private String clear = "\033[2J";
+    private String hide = "\033[?25l";
+    private String show = "\033[?25h";
     private ArrayDeque<OP> frontier = new ArrayDeque<OP>();
+    private MyPriorityQueue<OP> Frontier = new MyPriorityQueue<OP>();
     private int[] solutionCoordinates;
-    
     public Mazesolver(String filename){
 	startx = -1;
 	starty = -1;
+	endx = -1;
+	endy = -1;
 	//read the whole maze into a single string first
 	String ans = "";
 	try{
 	    Scanner in = new Scanner(new File(filename));
-	    
 	    //keep reading next line
 	    while(in.hasNext()){
 		String line = in.nextLine();
@@ -37,7 +37,6 @@ public class Mazesolver{
 	    e.printStackTrace();
 	    System.exit(0);
 	}
-	
 	//copy from the single string to a 2D array
 	maze = new char[maxx][maxy];
 	for(int i = 0; i < ans.length(); i++){
@@ -47,26 +46,25 @@ public class Mazesolver{
 		startx = i % maxx;
 		starty = i / maxx;
 	    }
+	    if(c == 'E'){
+		endx = i % maxx;
+		endy = i / maxx;
+	    }
 	}
 	OP start = new OP(startx, starty,0);
 	frontier.add(start);
-	
+	Frontier.add(start,0);
     }
-    
     //terminal specific character to move the cursor
     private String go(int x,int y){
 	return ("\033[" + x + ";" + y + "H");
     }
-    
-    
     private String color(int foreground,int background){
 	return ("\033[0;" + foreground + ";" + background + "m");
     }
-    
     public void clearTerminal(){
 	System.out.println(clear);
     }
-    
     public void wait(int millis){
 	try {
 	    Thread.sleep(millis);
@@ -74,27 +72,24 @@ public class Mazesolver{
 	catch (InterruptedException e) {
 	}
     }
-    
     public String toString(){
-    	String ans = "Solving a maze that is " + maxx + " by " + maxy + "\n";
-    	for(int i = 0; i < maxx * maxy; i++){
-    	    if(i % maxx == 0 && i != 0){
-    		ans += "\n";
-    	    }
-    	    char c =  maze[i % maxx][i / maxx];
-    	    if(c == '#'){
-    		ans += c;
-    	    }else{
-    		ans += c;
-    	    }
-    	}
-    	return hide + go(0,0) + ans + "\n" + show + color(37,40);
+	String ans = "Solving a maze that is " + maxx + " by " + maxy + "\n";
+	for(int i = 0; i < maxx * maxy; i++){
+	    if(i % maxx == 0 && i != 0){
+		ans += "\n";
+	    }
+	    char c = maze[i % maxx][i / maxx];
+	    if(c == '#'){
+		ans += c;
+	    }else{
+		ans += c;
+	    }
+	}
+	return hide + go(0,0) + ans + "\n" + show + color(37,40);
     }
-
     public boolean solveBFS(){
 	return solveBFS(false);
     }
-    
     public boolean solveBFS(boolean Animate){
 	char[][] Maze = new char[maze.length][maze[0].length];
 	OP placeHolder;
@@ -132,7 +127,7 @@ public class Mazesolver{
 		frontier.add(nextMove);
 	    }
 	    if (maze[current.getRow()][current.getCol() - 1] == ' ' || maze[current.getRow()][current.getCol() - 1] == 'E'){
-		OP nextMove = new OP(current.getRow(), current.getCol() - 1,current.getDistance() +  1, current);
+		OP nextMove = new OP(current.getRow(), current.getCol() - 1,current.getDistance() + 1, current);
 		frontier.add(nextMove);
 	    }
 	}
@@ -155,11 +150,9 @@ public class Mazesolver{
 	System.out.println(toString());
 	return Animate;
     }
-    
     public boolean solveDFS(){
 	return solveDFS(false);
     }
-
     public boolean solveDFS(boolean Animate){
 	char[][] Maze = new char[maze.length][maze[0].length];
 	OP placeHolder;
@@ -197,7 +190,7 @@ public class Mazesolver{
 		frontier.add(nextMove);
 	    }
 	    if (maze[current.getRow()][current.getCol() - 1] == ' ' || maze[current.getRow()][current.getCol() - 1] == 'E'){
-		OP nextMove = new OP(current.getRow(), current.getCol() - 1,current.getDistance() +  1, current);
+		OP nextMove = new OP(current.getRow(), current.getCol() - 1,current.getDistance() + 1, current);
 		frontier.add(nextMove);
 	    }
 	}
@@ -220,8 +213,141 @@ public class Mazesolver{
 	System.out.println(toString());
 	return Animate;
     }
-    
+    public boolean solveBest(){
+	return solveBest(false);
+    }
+    public boolean solveBest(boolean Animate){
+	char[][] Maze = new char[maze.length][maze[0].length];
+	OP placeHolder;
+	int rowC = 0;
+	int colC = 0;
+	while (rowC < maze.length){
+	    while (colC < maze[0].length){
+		Maze[rowC][colC] = maze[rowC][colC];
+		colC += 1;
+	    }
+	    rowC += 1;
+	    colC = 0;
+	}
+	while (true == true){
+	    if (Animate){
+		System.out.println(toString());
+		wait(20);
+	    }
+	    OP current = Frontier.removeSmallest();
+	    if (maze[current.getRow()][current.getCol()] == 'E'){
+		placeHolder = current;
+		break;
+	    }
+	    maze[current.getRow()][current.getCol()] = 'x';
+	    
+	    if (maze[current.getRow() + 1][current.getCol()] == ' ' || maze[current.getRow() + 1][current.getCol()] == 'E'){
+		OP nextMove = new OP(current.getRow() + 1,current.getCol(),current.getDistance() + 1,calculateDistanceFromEnd(current.getRow() + 1, current.getCol(), endx, endy) , current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+		
+	    }
+	    if (maze[current.getRow()][current.getCol() + 1] == ' ' || maze[current.getRow()][current.getCol() + 1] == 'E'){
+		OP nextMove = new OP(current.getRow() + 1,current.getCol(),current.getDistance() + 1,calculateDistanceFromEnd(current.getRow(), current.getCol() + 1, endx, endy), current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	    if (maze[current.getRow() - 1][current.getCol()] == ' ' || maze[current.getRow() - 1][current.getCol()] == 'E'){
+		OP nextMove = new OP(current.getRow() - 1,current.getCol(),current.getDistance() + 1,calculateDistanceFromEnd(current.getRow() - 1, current.getCol(), endx, endy), current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	    if (maze[current.getRow()][current.getCol() - 1] == ' ' || maze[current.getRow()][current.getCol() - 1] == 'E'){
+		OP nextMove = new OP(current.getRow(),current.getCol() - 1,current.getDistance() + 1,calculateDistanceFromEnd(current.getRow(), current.getCol() - 1, endx, endy), current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	}
+	Frontier.clear();
+	int size = (placeHolder.getDistance() + 1) * 2;
+	solutionCoordinates = new int[size];
+	size -= 1;
+	while (size >= 0){
+	    solutionCoordinates[size] = placeHolder.getCol();
+	    size -= 1;
+	    solutionCoordinates[size] = placeHolder.getRow();
+	    size -= 1;
+	    Maze[placeHolder.getRow()][placeHolder.getCol()] = 'x';
+	    if (placeHolder.getPrevious() == null){
+		break;
+	    }
+	    placeHolder = placeHolder.getPrevious();
+	}
+	maze = Maze;
+	System.out.println(toString());
+	return Animate;
+	
+    }
+    public boolean solveAStar(){
+	return solveBest(false);
+    }
+    public boolean solveAStar(boolean Animate){
+	char[][] Maze = new char[maze.length][maze[0].length];
+	OP placeHolder;
+	int rowC = 0;
+	int colC = 0;
+	while (rowC < maze.length){
+	    while (colC < maze[0].length){
+		Maze[rowC][colC] = maze[rowC][colC];
+		colC += 1;
+	    }
+	    rowC += 1;
+	    colC = 0;
+	}
+	while (true == true){
+	    if (Animate){
+		System.out.println(toString());
+		wait(20);
+	    }
+	    OP current = Frontier.removeSmallest();
+	    if (maze[current.getRow()][current.getCol()] == 'E'){
+		placeHolder = current;
+		break;
+	    }
+	    maze[current.getRow()][current.getCol()] = 'x';
+	    if (maze[current.getRow() + 1][current.getCol()] == ' ' || maze[current.getRow() + 1][current.getCol()] == 'E'){
+		OP nextMove = new OP(current.getRow() + 1,current.getCol(),current.getDistance() + 1,calculateDistanceFromEnd(current.getRow() + 1, current.getCol(), endx, endy) + current.getDistance() + 1, current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	    if (maze[current.getRow()][current.getCol() + 1] == ' ' || maze[current.getRow()][current.getCol() + 1] == 'E'){
+		OP nextMove = new OP(current.getRow() + 1,current.getCol(),current.getDistance() + 1,calculateDistanceFromEnd(current.getRow(), current.getCol() + 1, endx, endy) + current.getDistance() + 1, current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	    if (maze[current.getRow() - 1][current.getCol()] == ' ' || maze[current.getRow() - 1][current.getCol()] == 'E'){
+		OP nextMove = new OP(current.getRow() - 1,current.getCol(),current.getDistance() + 1,calculateDistanceFromEnd(current.getRow() - 1, current.getCol(), endx, endy) + current.getDistance() + 1, current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	    if (maze[current.getRow()][current.getCol() - 1] == ' ' || maze[current.getRow()][current.getCol() - 1] == 'E'){
+		OP nextMove = new OP(current.getRow(),current.getCol() - 1,current.getDistance() + 1,calculateDistanceFromEnd(current.getRow(), current.getCol() - 1, endx, endy) + current.getDistance() + 1, current);
+		Frontier.add(nextMove,nextMove.getDistanceFromEnd());
+	    }
+	}
+	Frontier.clear();
+	int size = (placeHolder.getDistance() + 1) * 2;
+	solutionCoordinates = new int[size];
+	size -= 1;
+	while (size >= 0){
+	    solutionCoordinates[size] = placeHolder.getCol();
+	    size -= 1;
+	    solutionCoordinates[size] = placeHolder.getRow();
+	    size -= 1;
+	    Maze[placeHolder.getRow()][placeHolder.getCol()] = 'x';
+	    if (placeHolder.getPrevious() == null){
+		break;
+	    }
+	    placeHolder = placeHolder.getPrevious();
+	}
+	maze = Maze;
+	System.out.println(toString());
+	return Animate;
+	
+    }
     public int[] solutionCoordinates(){
 	return solutionCoordinates;
+    }
+
+    public int calculateDistanceFromEnd(int currentX, int currentY, int endX, int endY){
+	return Math.abs(currentX - endX) + Math.abs(currentY - endY);
     }
 }
