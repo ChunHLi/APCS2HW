e1aa6a06488979e581c5ed506d4a54faf203785b
Shawn Li <shawnli050298@gmail.com>
2015-05-18 18:43:02 -0400

    Get sorted scrubLord

--- 14SittingRange/HeapSort.java
+++ 14SittingRange/HeapSort.java
@@ -1,10 +1,45 @@
 public class HeapSort{
-    public static void heapSort(int[] Array){
-    }
+    private static int Size;
+    
+    public static void heapSort(int[] Array){       
+	int counter = Size;
+	heapify(Array);
+	while (counter > 0){
+            swap(Array, 0, counter);
+            maxheap(Array, 0);
+	    Size -= 1;
+	    counter -= 1;
+        }	
+    }     
 
-    public int[] heapify(int[] Array, int index){
+    public static void heapify(int[] Array){
+        Size = Array.length - 1;
+	int counter = Size/2;
+	while (counter >= 0){
+            maxheap(Array, counter); 
+	    counter -= 1;
+	}
     }
+    
+    public static void maxheap(int[] Array, int Index){ 
+        int left = Index * 2;
+        int right = Index * 2 + 1;
+        int Largest = Index;
+        if (left <= Size && Array[left] > Array[Index]){
+            Largest = left;
+	}
+        if (right <= Size && Array[right] > Array[Largest]){    
+            Largest = right;
+	}
+        if (Largest != Index){
+            swap(Array, Index, Largest);
+            maxheap(Array, Largest);
+        }
+    }    
 
-    public int remove(int[] Array){
-    }
+    public static void swap(int[] Array, int firstIndex, int secondIndex){
+        int temp = Array[firstIndex];
+        Array[firstIndex] = Array[secondIndex];
+        Array[secondIndex] = temp; 
+    }    
 }
